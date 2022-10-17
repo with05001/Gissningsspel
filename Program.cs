3996a0a20f535d4e0014d91165f994983674af61
with05001 <with05001@utb.vaxjo.se>
2022-10-17 08:46:57 +0200

    Skapat ett gissningsspel

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Random Rnd = new Random();
+int Rätt, input;
+Rätt = Rnd.Next(101);
+int Räknare = 0;
+do{
+    Console.WriteLine("Gissa ett tal från 1-100");
+    input = int.Parse(Console.ReadLine());
+    if(input<Rätt){
+        Console.WriteLine("Tallet är för lågt");
+    }
+    else if(input>Rätt){
+        Console.WriteLine("Tallet är för högt");
+    }
+    else{
+        Console.WriteLine("Du gissade rätt!");
+    }
+    Räknare++;
+}while(input!=Rätt);
+Console.WriteLine("Det tog dig "+ Räknare +" försök!");
